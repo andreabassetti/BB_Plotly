30909a47499d09f372fcd43687c3c6ca344e8b9f
andreabassetti <andreajr@bassetti.net>
2022-01-04 11:21:26 -0800

    12.4

---
+++ Module Practice/spaceX.js
@@ -0,0 +1,32 @@
+const url = "https://api.spacexdata.com/v2/launchpads";
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+researcher1 = [['name', 'Roza'], ['age', 34], ['hobby',
+'Hiking']];
+
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
